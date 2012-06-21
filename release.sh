53ae7e4752adbc52766ea08fb0a75a1553067722
David Winterbottom <david.winterbottom@gmail.com>
2012-06-21 10:32:06 +0100

    Modified release script

--- release.sh
+++ release.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 # Cut a release to PyPi and update Github with tag.
 
-# Ensure there are no uncomitted changes
-git diff --quiet HEAD
-[ $? -ne 0 ] && echo "Uncommitted changes!" && exit 1
-
-# Pluck release number out of setup.py
-RELEASE_NUM=`grep version setup.py | cut -d\' -f2`
-git tag | grep $RELEASE_NUM > /dev/null && \
-	echo "New version number required ($RELEASE_NUM already used)" && exit 1
-
 # Push to PyPi
 ./setup.py sdist upload
 
 # Tag in Git
-git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
 git push origin master
 git push --tags
