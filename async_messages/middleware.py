edfb95147eb96cda31fea2fe8a602824519daee0
twidi <s.angel@twidi.com>
2013-09-03 00:13:18 +0200

    Correct indentation in middleware.py

--- async_messages/middleware.py
+++ async_messages/middleware.py
@@ -8,9 +8,9 @@ class AsyncMiddleware(object):
     def process_response(self, request, response):
         # Check for messages for this user and, if it exists,
         # call the messages API with it
-       if hasattr(request, "session") and request.user.is_authenticated():
+        if hasattr(request, "session") and request.user.is_authenticated():
             msgs = get_messages(request.user)
             if msgs:
                 for msg, level in msgs:
                     messages.add_message(request, level, msg)
-       return response
+        return response
